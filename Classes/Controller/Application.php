b047e2b9028a98dad2e0969c4d65249da24dda6a
Dimitri Ebert <dimitri.ebert@dkd.de>
2015-09-07 15:20:13 +0200

    Basic structure

---
+++ Classes/Controller/Application.php
@@ -0,0 +1,11 @@
+<?php
+namespace Dkd\Fishlike\Controller;
+
+use TYPO3\CMS\Extensionmanager\Controller\ActionController;
+
+/**
+ * Class ArchiveController
+ */
+class ApplicationController extends ActionController {
+
+}
---
+++ ext_tables.sql
@@ -0,0 +1,11 @@
+#
+# Table structure for table 'tx_fishlike_like'
+#
+CREATE TABLE tx_fishlike_like (
+    uid int(11) NOT NULL auto_increment,
+    item_type varchar(255) DEFAULT '' NOT NULL,
+    counter  int(11) NOT NULL DEFAULT 0,
+    PRIMARY KEY (uid),
+    KEY processed (processed),
+    KEY item_uuid (item_type,item_uuid)
+) ENGINE=InnoDB;
